692bab3eb01c6484399b4ec2476675d1859cf6e8
Evill <anylight5@yandex.ru>
2017-06-20 19:36:24 +0300

    Exact change - replaced switch with object lookup and removed comments;

--- exactChange.js
+++ exactChange.js
@@ -3,9 +3,7 @@ function checkCashRegister(price, cash, cid) {
   var totalChange = 100* (cash - price); //in cents
   var totalCid = totalDrawer(cid);
   var result = [];
-  //console.log(totalCid);
   var coinsLeft = totalChange;
-  // Here is your change, ma'am.
   if (totalChange > totalCid){
     return "Insufficient Funds";
   }
@@ -42,39 +40,11 @@ function checkCashRegister(price, cash, cid) {
     }
     return total;
   }
+
   function getVal(value){
-    switch (value) {
-      case "PENNY":
-        return 1;
-      case "PENNY":
-        return 5;
-      case "DIME":
-        return 10;
-      case "QUARTER":
-        return 25;
-      case "ONE":
-        return 100;
-      case "FIVE":
-        return 500;
-      case "TEN":
-        return 1000;
-      case "TWENTY":
-        return 2000;
-      case "ONE HUNDRED":
-        return 10000;
-    }
+    var pairs = {"PENNY":1, "NICKEL": 5, "DIME":10, "QUARTER" : 25,"ONE":100, "FIVE":500, "TEN": 1000, "TWENTY": 2000, "ONE HUNDRED" : 10000};
+    return pairs[value];
   }
 }
-
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.10],
-// ["QUARTER", 4.25],
-// ["ONE", 90.00],
-// ["FIVE", 55.00],
-// ["TEN", 20.00],
-// ["TWENTY", 60.00],
-// ["ONE HUNDRED", 100.00]]
 
 checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
